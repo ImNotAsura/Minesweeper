5c89d1ae496bfd3f94df33d1752e0dc0046b5651
marcusawd <angweidemarcus@gmail.com>
2023-08-24 16:03:32 +0800

    Checkpoint

--- main.js
+++ main.js
@@ -5,11 +5,25 @@ const cols = 8;
 const numMines = 10;
 
 /*----- state variables -----*/
+let minefield = [];
+
+for (let i = 0; i < rows; i++) {
+	minefield[i] = [];
+	for (let j = 0; j < cols; j++) {
+		minefield[i][j] = {};
+	}
+}
+console.log(minefield);
 
 /*----- cached elements -----*/
 const board = document.querySelector("#board");
 
 /*----- event listeners -----*/
+const handleCellClick = (row, col) => {
+	console.log(row, col);
+};
+
+const handleRightClick = (row, col) => {};
 
 /*----- render functions -----*/
 const renderBoard = () => {
@@ -18,6 +32,20 @@ const renderBoard = () => {
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < cols; j++) {
 			const cell = document.createElement("div");
+			cell.classList.add("cell");
+
+			cell.addEventListener("click", () => {
+				handleCellClick(i, j);
+				renderBoard();
+			});
+
+			cell.addEventListener("mousedown", (event) => {
+				if (event.button === 2) {
+					event.preventDefault();
+					handleRightClick(i, j);
+					renderBoard();
+				}
+			});
 
 			board.appendChild(cell);
 		}
