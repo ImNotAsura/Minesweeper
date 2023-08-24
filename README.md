c71a585617fcd7232bab68202961b652da8b5c4f
marcusawd <angweidemarcus@gmail.com>
2023-08-24 10:19:10 +0800

    Updated readme, did some boilerplate for js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Minesweeper
+
+3 different rendered states for the game
+Choosing screen - Ability to choose difficulty, from drop down menu, or input your own rows and cols and choose amount of bombs -> Be it % of board or number
+Game itself -> Render board
+Ending - How many easy/med/hard was completed so far. (Custom sizing will have its own LB) and the time taken to complete the game (Will need to implement timer during the game itself)
+
+How to play minesweeper:
+Left click on an un-opened cell to reveal. (Underneath will be a number or a bomb)
+For a number -> The cells surrounding it will consist of the number of bombs === number
+Right click to set a flag on a cell that you think is a bomb
+Flood feature will automatically reveal empty spaces that will not have number or bomb -> On successful left click
+
+MVP
+Able to render a board and able to click on the cells, with game ending when bomb is clicked
--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+/*----- constants -----*/
+//* Fixed constants for now, will allow for user input in the future
+const rows = 8;
+const cols = 8;
+const numMines = 10;
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/*----- state variables -----*/
 
-setupCounter(document.querySelector('#counter'))
+/*----- cached elements -----*/
+const board = document.querySelector("#board");
+
+/*----- event listeners -----*/
+
+/*----- render functions -----*/
+const renderBoard = () => {
+	board.innerHTML = "";
+
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			const cell = document.createElement("div");
+
+			board.appendChild(cell);
+		}
+	}
+};
+
+/*----- game logic functions -----*/
+const initBoard = () => {
+	//* Initialise the minefield and place mines
+	placeMines();
+};
+
+const placeMines = () => {};
+
+function init() {
+	initBoard();
+	renderBoard();
+}
+
+init();
